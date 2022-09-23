3ab3b712107b8c4af96842f6ce9168bfb92478da
Olami07 <daisi4real@gmail.com>
2022-09-23 15:57:43 +0100

    First Java commit

---
+++ JenkinsMain.java
@@ -0,0 +1,15 @@
+// Your First Program
+
+class JenkinsMain {
+    public static void main(String[] args) {
+        int b;
+        int i;
+        i= 35;
+        b = 78;
+        int a =b*i;
+        System.out.println(a); 
+    }
+
+    public JenkinsMain() {
+    }
+}
